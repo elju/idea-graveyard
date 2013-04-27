c035dc44a1eb06a7beab6c608d708bae295de1ba
Luke Underwood <nonintuitive@lju.(none)>
2013-04-27 11:24:15 -0500

    new file:   app.js 	modified:   package.json 	new file:   public/images/Painting_1.jpg 	new file:   public/images/background.jpg 	new file:   public/javascripts/jquery-2.0.0.min.js 	new file:   public/javascripts/main.js 	new file:   public/stylesheets/extra.css 	new file:   public/stylesheets/extra.less 	new file:   public/stylesheets/github.css 	new file:   public/stylesheets/main.css 	new file:   public/stylesheets/style.less 	new file:   routes/index.js 	new file:   routes/user.js 	new file:   views/art.jade 	new file:   views/code.jade 	new file:   views/index.jade 	new file:   views/navbar.jade 	new file:   views/other.jade

---
+++ public/javascripts/main.js
@@ -0,0 +1,69 @@
+$(function() {
+    console.time("setting up click events.");
+    $(".navbutton").each(function(i, el) {
+        that = this;
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            history.pushState({}, this.id, this.href);
+            $mainbar = $('.mainbar');
+            $mainbar.hasClass('activated') ? $mainbar.removeClass('activated') : $mainbar.addClass('activated');
+            return false;
+        })
+    });
+    console.timeEnd("setting up click events.");
+});
+
+$(function() {
+    var $subwindows;
+    $subwindows = $('.subwindow');
+    $subwindows.on('click', function(e) {
+        e.stopPropagation();
+        var $this, $right, $left, $others;
+        $this = $(this);
+        if ($this.hasClass('selected')) {
+            return;
+        }
+        $subwindows.removeClass('selected');
+        $this.removeClass('otherSelected').addClass('selected');
+        $right = $subwindows.filter('div.selected ~ div');
+        $left = $subwindows.not($right).not(this);
+        $others = $left.add($right);
+        $others.addClass('otherSelected');
+        $left.addClass('left');
+        $right.addClass('right');
+    });
+});
+
+
+
+/*
+getCode = function(e) {
+    var repos, req;
+    req = new XMLHttpRequest();
+    req.open('GET', 'https://api.github.com/user/repos');
+    req.setRequestHeader('Authorization', 'token 8f8f36e3b95c020f4bf7679c48609bac4576fec5');
+    req.onload = codeLoaded.bind(this);
+    req.send();
+
+    function codeLoaded() {
+        if (req.status == 200) {
+            repos = eval(req.response);
+            console.log(repos);
+        } else {
+            console.log('Some type of problem with the AJAX request!');
+            console.log(req); }
+
+        function populateMainbar(repos) {
+            var links, mainbar;
+            mainbar = document.getElementById('mainbar');
+            repos.forEach( function(repo) {
+                var link;
+                link = document.createElement('a');
+                link.id = repo.name;
+                link.setAttribute('class', 'repo');
+                mainbar.app
+                
+            });
+    }
+};
+*/
