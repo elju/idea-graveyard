cf92727da5c824d5c736c7cdc2a63dc520a529d0
Luke Underwood <nonintuitive@lju.(none)>
2013-05-01 12:05:10 -0500

    modified:   server.js 	modified:   views/code.jade 	modified:   views/index.jade

--- server.js
+++ server.js
@@ -4,13 +4,41 @@
  */
 
 var express = require('express')
+  , exec = require('child_process').exec
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
-  , fs = require('fs');
+  , fs = require('fs')
+  , GithubAPI = require('github');
 
 var app = express();
+
+var ls = exec('ls -la', function(err, stdout, stderr) {
+    console.log('Results from the ls: ' + stdout);
+    if (err) console.log('There was en error ' + err);
+});
+
+var listRepoNames = function listRepoNames (arr) {
+    arr.forEach(function(elt, i) {
+        console.log('Repo ' + i + ': ' + elt.name);
+    });
+};
+
+var github = new GithubAPI({
+    version: "3.0.0",
+    timeout: 5000
+});
+github.authenticate({
+    type: "oauth",
+    token: "843fcb5c469b46a4f8e0c51ce32935e0db3e3a0b"
+});
+github.repos.getAll({}, function(err, res) {
+    console.log("Here's the response! :");
+    listRepoNames(res);
+});
+
+
 
 // all environments
 
@@ -20,14 +48,14 @@ var dirz = (process.env.OPENSHIFT_REPO_DIR || __dirname);
 app.set('port', portz || 3000);
 app.set('views', dirz + '/views');
 app.set('view engine', 'jade');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-  app.use(require('less-middleware')({ src: process.env.OPENSHIFT_REPO_DIR + '/public' }));
+app.use(require('less-middleware')({ src: (process.env.OPENSHIFT_REPO_DIR || '.') + '/public' }));
 app.use(express.static(dirz + '/public'));
-console.log(dirz);
 
 // development only
 if ('development' == app.get('env')) {
